6ff1c7a33e90e4953cb9c6d5df990a474a951296
experiment1234 <experiment123@naver.com>
2023-09-11 17:43:35 +0900

    20230911

--- 01_servlet/src/main/java/ex02_request/RequestServlet.java
+++ 01_servlet/src/main/java/ex02_request/RequestServlet.java
@@ -1,6 +1,7 @@
 package ex02_request;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Optional;
 
 import javax.servlet.ServletException;
@@ -72,7 +73,17 @@ public class RequestServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+				response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	request.setCharacterEncoding("UTF-8");
+	
+	//2. 동일한 요청 파라미터가 2개 이상 전달되는 경우 getParameterValues()메소드를 이용해서 파라미터를 배열로 받는다. 
+	String[] tel = request.getParameterValues("tel");
+	String[] hobbies = request.getParameterValues("hobbies");
+	
+	System.out.println(Arrays.toString(tel));
+	System.out.println(Arrays.toString(hobbies));
+	
+	
 	}
-
 }
